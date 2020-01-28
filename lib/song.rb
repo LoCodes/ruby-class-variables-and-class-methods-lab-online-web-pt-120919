58e079d52a9990cf5326875e70e879efad6c8a4a
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-28 04:48:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song  
+  attr_accessor :name, :artist, :genre #takes in three arguments: a name, artist and genre
+  
+  @@count = 0  
+  @@genres = []   
+  @@artists = []
+  
+  def initialize (name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1
+    @@artists << artist 
+    @@genres << genre 
+  end
+    
+  def self.count
+    @@count
+  end
+  
+  def self.artists 
+    @@artists.uniq 
+  end
+  
+  
+  def self.genres 
+    @@genres.uniq 
+  end
+  
+  def self.genre_count
+    genre_count = {}
+  end
+
+
+  
+end
+
